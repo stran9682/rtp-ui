26ba9845a063605d58d763eecd444dc03e86c144
Sebastian Tran <transebastian21@gmail.com>
2026-01-07 01:14:42 -0500

    Initial Commit

---
+++ rtp-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rtp-ui
+//
+//  Created by Sebastian Tran on 1/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
